baaeda38bdee850cd6e0a24edde8294ddcc817e0
yunho choi <choiking10@naver.com>
2021-04-15 17:56:54 +0900

    Implement step01

---
+++ example/step01.py
@@ -0,0 +1,24 @@
+import numpy as np
+from mytorch.variable import Variable
+
+data = np.array(1.0)
+x = Variable(data)
+print(x.data)
+
+x.data = np.array(2.0)
+print(x.data)
+
+###
+# supplement
+# 다차원배열 = tensor
+###
+
+x = np.array(1)
+print(x.ndim)  # ndim = number of dimensions
+
+x = np.array([1, 2, 3])
+print(x.ndim)
+
+x = np.array([[1, 2, 3], [4, 5, 6]])
+print(x.ndim)
+
