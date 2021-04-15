bad06d51a3c1192327b8f442af3e44e17856a46d
yunho choi <choiking10@naver.com>
2021-04-15 19:54:19 +0900

    step07 - Auto backpropagation

--- mytorch/function.py
+++ mytorch/function.py
@@ -25,7 +25,9 @@ class Function:
         x = _input.data
         y = self.forward(x)
         output = Variable(y)
+        output.set_creator(self)
         self.input = _input
+        self.output = output
         return output
 
     def forward(self, x):
