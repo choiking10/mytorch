350806e76e91f2bd70f5c9e2f04e93e923691fb1
yunho choi <choiking10@naver.com>
2021-04-15 20:05:54 +0900

    step08 - change backward method from reculsive to iterative

---
+++ example/step08.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+from mytorch import function as F
+from mytorch.variable import Variable
+
+A = F.Square()
+B = F.Exp()
+C = F.Square()
+
+# y = (e^{x^2})^2
+# 계산 그래프의 연결 x -> A -> a -> B -> b -> C -> y
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+y.grad = np.array(1.0)
+y.backward()
+print(x.grad)
