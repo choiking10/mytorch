b6427530047a0227055523080db400cf4322db1a
yunho choi <choiking10@naver.com>
2021-04-16 10:03:20 +0900

    Add ExpTest and FuntionTestMixin.

--- tests/test_function.py
+++ tests/test_function.py
@@ -7,25 +7,62 @@ from mytorch.utils import numerical_diff
 from mytorch import function as F
 
 
-class SquareTest(unittest.TestCase):
+class FunctionTestMixin:
+    def get_function(self):
+        raise NotImplementedError()
+
+    def get_forward_input_output(self):
+        raise NotImplementedError()
+
+    def get_backward_input_output(self):
+        raise NotImplementedError()
+
     def test_forward(self):
-        x = Variable(np.array(2.0))
-        y = F.square(x)
-        expected = np.array(4.0)
-        self.assertEqual(y.data, expected)
+        x, expected_y = self.get_forward_input_output()
+        f = self.get_function()
+        x = Variable(x)
+        y = f(x)
+        self.assertEqual(y.data, expected_y)
 
     def test_backward(self):
-        x = Variable(np.array(3.0))
-        y = F.square(x)
+        x, expected_grad = self.get_backward_input_output()
+        f = self.get_function()
+        x = Variable(x)
+        y = f(x)
         y.backward()
-        expected = np.array(6.0)
-        self.assertEqual(x.grad, expected)
+        self.assertEqual(x.grad, expected_grad)
 
     def test_gradient_check(self):
         x = Variable(np.random.rand(1))
-        y = F.square(x)
+        f = self.get_function()
+        y = f(x)
         y.backward()
-        num_grad = numerical_diff(F.square, x)
+        num_grad = numerical_diff(f, x)
         flg = np.allclose(x.grad, num_grad)
         self.assertTrue(flg)
 
+
+class SquareTest(unittest.TestCase, FunctionTestMixin):
+    def get_function(self):
+        return F.square
+
+    def get_forward_input_output(self):
+        return np.array(2.0), np.array(4.0)
+
+    def get_backward_input_output(self):
+        return np.array(3.0), np.array(6.0)
+
+
+class ExpTest(unittest.TestCase, FunctionTestMixin):
+    def get_function(self):
+        return F.exp
+
+    def get_forward_input_output(self):
+        x = np.array(2.0)
+        y = np.array(np.exp(x))
+        return x, y
+
+    def get_backward_input_output(self):
+        x = np.array(3.0)
+        grad_y = np.array(np.exp(x))
+        return x, grad_y
