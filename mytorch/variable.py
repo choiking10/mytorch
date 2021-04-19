b7f940e559df169a18dd69561034042dcf094833
yunho choi <choiking10@naver.com>
2021-04-19 20:01:14 +0900

    step19 Comfortable variables.

--- mytorch/variable.py
+++ mytorch/variable.py
@@ -4,11 +4,12 @@ import numpy as np
 
 
 class Variable:
-    def __init__(self, data):
+    def __init__(self, data, name=None):
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError(f'{type(data)} is not supported.')
         self.data = data
+        self.name = name
         self.grad = None
         self.creator = None
         self.generation = 0  # 세대 수를 기록하는 변수
@@ -49,5 +50,28 @@ class Variable:
     def zerograd(self):
         self.grad = None
 
+    @property
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def ndim(self):
+        return self.data.ndim
+
+    @property
+    def size(self):
+        return self.data.size
+
+    @property
+    def dtype(self):
+        return self.data.dtype
+
+    def __len__(self):
+        return len(self.data)
+
     def __repr__(self):
-        return f"data: {self.data} grad: {self.grad} creator: {self.creator} generation: {self.generation}"
+        if self.data is None:
+            p = 'None'
+        else:
+            p = str(self.data).replace('\n', '\n' + ' ' * 9)
+        return f'variable({p})'
